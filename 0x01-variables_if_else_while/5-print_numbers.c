ed8fc39f0ea04530b6566a68a6e9beff57d803bf
cort-robinson <cortrobinson99@gmail.com>
2020-06-11 20:24:18 +0000

    add 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print single digit numbers of base 10 followed by newline
+ * Return: 0 if no errors
+ **/
+
+int main(void)
+{
+	int i = 1;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
