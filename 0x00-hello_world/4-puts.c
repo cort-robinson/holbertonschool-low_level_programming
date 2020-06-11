7f2c911cd5d67e0fca949818424611940a9c3316
cort-robinson <cortrobinson99@gmail.com>
2020-06-11 02:30:38 +0000

    changed 4-puts.c to comply with betty linter

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
+/**
+ * main - outputs a string
+ * Return: 0 if code returns no erros
+ */
+
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-  return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
