dd06e4d143052e413e127579cc9f07134c0b2c75
cort-robinson <cortrobinson99@gmail.com>
2020-06-17 05:05:06 +0000

    add 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+ * add - adds two integers and returns result
+ * @x: first integer
+ * @y: second integer
+ * Return: sum of integers
+ */
+
+int add(int x, int y)
+{
+	return (x + y);
+}
