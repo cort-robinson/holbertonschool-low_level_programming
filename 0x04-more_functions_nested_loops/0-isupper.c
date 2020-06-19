0e45226b21d2c150b2f3999d47c6f09715813fb1
cort-robinson <cortrobinson99@gmail.com>
2020-06-19 03:43:59 +0000

    change to 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,13 +3,17 @@
 /**
  * _isupper - checks for uppercase character
  * @c: variable to check
- * Return; 1 if uppercase, else 0
+ * Return: 1 if uppercase, else 0
  */
 
 int _isupper(int c)
 {
 	if (c >= 'A' && c <= 'Z')
+	{
 		return (1);
-	if (c >= 'a' && c <= 'z')
+	}
+	else
+	{
 		return (0);
+	}
 }
