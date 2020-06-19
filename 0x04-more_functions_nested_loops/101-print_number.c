37a36da1dc425d01f428e8a83c85007f1a34e176
cort-robinson <cortrobinson99@gmail.com>
2020-06-19 18:51:45 +0000

    update holberton.h and add 101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ * print_number - Prints an integer.
+ * @n: integer to print.
+ * Return: void.
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+
+	if (n / 10)
+		print_number(n / 10);
+	_putchar('0' + n % 10);
+}
