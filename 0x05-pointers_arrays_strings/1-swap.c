ab7a065e4c4c643de98aebcb5708b5c042165d2e
cort-robinson <cortrobinson99@gmail.com>
2020-06-23 20:03:01 +0000

    add 1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * swap_int - Swaps the values of two integers.
+ * @a: Variable int 1.
+ * @b: variable int 2.
+ * Return: Void.
+ */
+void swap_int(int *a, int *b);
+{
+	int temp_assignment = *a;
+
+	*a = *b;
+	*b = *temp_assignment;
+}
