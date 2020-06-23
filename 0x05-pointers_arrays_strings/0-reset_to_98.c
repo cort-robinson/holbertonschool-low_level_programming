17f50aadbfdd66cf1442609754262a9157bbe7d3
cort-robinson <cortrobinson99@gmail.com>
2020-06-23 19:42:21 +0000

    fix comments in 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -4,7 +4,7 @@
  * reset_to_98 - Takes a pointer to an int as parameter and
  * updates the value it points to to 98.
  * @n: Pointer.
- * Return: Always 0.
+ * Return: Void.
  */
 
 void reset_to_98(int *n)
