3689acaa0c0abf21a2dd802e18af427ed3543697
cort-robinson <cortrobinson99@gmail.com>
2020-06-23 20:31:06 +0000

    add 2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * _strlen - Returns the length of a string.
+ * @s: String to be checked for length.
+ * Return: Length of string.
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+	while (s[length] != '\0')
+		length++;
+	return (length);
+}
