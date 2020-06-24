7c81a816dd8afad6fb91c090bc4fd883787660d4
cort-robinson <cortrobinson99@gmail.com>
2020-06-24 03:10:12 +0000

    fix 9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -9,12 +9,12 @@
  */
 char *_strcpy(char *dest, char *src)
 {
-	while (*src)
+	int i;
+
+	for (i = 0; src[i]; i++)
 	{
-		*dest = *src;
-		src++;
-		dest++;
+		dest[i] = src[i];
 	}
-	*dest = '\0';
+	dest[i] = '\0';
 	return (dest);
 }
