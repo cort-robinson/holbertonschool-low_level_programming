911ae179376eab7467737eeb354c2bd479c992f3
cort-robinson <cortrobinson99@gmail.com>
2020-06-26 04:48:34 +0000

    add 7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ * leet - Encodes a string to 1337/leet.
+ * @str: String to be encoded.
+ * Return: Encoded string.
+ */
+
+char *leet(char *str)
+{
+	int i, check;
+	char *ltr = "aAeEoOtTlL";
+	char *num = "4433007711";
+
+	for (i = 0; str[i]; i++)
+	{
+		for (check = 0; str[check]; check++)
+		{
+			if (str[i] == ltr[check])
+			{
+				str[i] = num[check];
+				break;
+			}
+		}
+	}
+	return (str);
+}
