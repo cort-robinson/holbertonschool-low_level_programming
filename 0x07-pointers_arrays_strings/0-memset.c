a2f48fdc08fcd2c12b9e17b3d85ba5be63bceb1d
cort-robinson <cortrobinson99@gmail.com>
2020-06-29 22:59:45 +0000

    change 0-memset.c for betty

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,14 +4,16 @@
  * _memset - Fills the first n bytes of the memory area.
  * @s: Points to memory area to be filled.
  * @b: Constant byte to fill memory area with.
+ * @n: Bytes of memory area to fill.
+ * Return: Pointer to memory area s.
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int i;
+	unsigned int i;
 
-    for (i = 0; i < n; i++)
-        s[i] = b;
+	for (i = 0; i < n; i++)
+		s[i] = b;
 
-    return (s);
+	return (s);
 }
