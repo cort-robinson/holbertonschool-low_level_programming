811f5057358e1bb373691199aa8e215c9b3f6d6c
cort-robinson <cortrobinson99@gmail.com>
2020-06-29 23:50:29 +0000

    add 3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,32 @@
+#include "holberton.h"
+
+/**
+ * _strspn - Gets the length of a prefix substring.
+ * @s: String to check.
+ * @accept: String to check chars from.
+ * Return: Number of bytes in the initial segment of
+ * s which consist only of bytes from accept.
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int i, check, temp, matches = 0;
+
+	for (i = 0; s[i]; i++)
+	{
+		temp = matches;
+		for (check = 0; accept[check]; check++)
+		{
+			if (s[i] == accept[check])
+			{
+				matches++;
+			}
+			
+		}
+		if (temp == matches)
+		{
+			break;
+		}
+	}
+	return (matches);
+}
