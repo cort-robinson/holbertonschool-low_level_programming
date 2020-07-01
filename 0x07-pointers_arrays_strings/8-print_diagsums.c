e74e3078d5b49d206e797367a11ebaa24d88a1f7
cort-robinson <cortrobinson99@gmail.com>
2020-07-01 23:08:34 +0000

    add 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "holberton.h"
 
 /**
@@ -10,5 +11,14 @@
 
 void print_diagsums(int *a, int size)
 {
+	int i = 0, sum1 = 0, sum2 = 0;
 
+	while (i < size)
+	{
+		sum1 += (a + i * size)[i];
+		sum2 += (a + i * size)[size - 1 - i];
+		i++;
+	}
+	printf("%d, %d", sum1, sum2);
+	printf("\n");
 }
