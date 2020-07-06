7d378eb360fc75d1a6b186e55604cfcec476331b
cort-robinson <cortrobinson99@gmail.com>
2020-07-06 01:10:23 +0000

    add 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - Prints the program's name.
+ * @argc: Count of arguments.
+ * @argv: Arguments used to call program.
+ * Return: 0 if no errors.
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
