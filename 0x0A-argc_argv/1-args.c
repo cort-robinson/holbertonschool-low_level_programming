d8d5273f4b935f277f24387d413d8b6d60e7084d
cort-robinson <cortrobinson99@gmail.com>
2020-07-06 01:30:52 +0000

    add 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - Prints the number of arguments passed into it.
+ * @argc: Argument count.
+ * Return: Number of arguments passed to program.
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
