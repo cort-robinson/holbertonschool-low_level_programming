e3594f36abdafc4e4b2ce9aa7c8b409fb17285d7
cort-robinson <cortrobinson99@gmail.com>
2020-07-06 01:45:26 +0000

    add 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * 
+ * 
+ * 
+ */
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	while (i < argc)
+	{
+		printf("%s\n", argv[i]);
+		i++;
+	}
+	return (0);
+}
