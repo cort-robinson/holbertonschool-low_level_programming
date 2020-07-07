0fd45cb1545d8b01ea4b8c87449e8ccd945b5feb
cort-robinson <cortrobinson99@gmail.com>
2020-07-07 22:41:22 +0000

    fix for check 12

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -18,11 +18,11 @@ char *create_array(unsigned int size, char c)
 		return (NULL);
 
 	ptr = (char *) malloc(size * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
 	for (i = 0; i < size; i++)
 		ptr[i] = c;
 
-	if (ptr == NULL)
-		return (NULL);
 	return (ptr);
 
 }
