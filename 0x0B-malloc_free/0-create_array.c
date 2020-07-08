9bf38d23abb23670af6b8d1ee27f790bfb7329ec
cort-robinson <cortrobinson99@gmail.com>
2020-07-08 18:33:50 +0000

    code cleanup

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -17,7 +17,7 @@ char *create_array(unsigned int size, char c)
 	if (size == 0)
 		return (NULL);
 
-	ptr = (char *) malloc(size * sizeof(char));
+	ptr = malloc(size * sizeof(char));
 	if (ptr == NULL)
 		return (NULL);
 	for (i = 0; i < size; i++)
