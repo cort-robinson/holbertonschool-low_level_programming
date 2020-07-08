79a86291424251666157e5301855c84cea47daed
cort-robinson <cortrobinson99@gmail.com>
2020-07-08 02:52:58 +0000

    changes to 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -23,9 +23,9 @@ int **alloc_grid(int width, int height)
 	if (ptr == NULL || *ptr == NULL)
 		return (NULL);
 
-	for (i = 0; i < height; i++)
+	for (i = 0; i < width; i++)
 	{
-		for (j = 0; j < width; j++)
+		for (j = 0; j < height; j++)
 		{
 			*ptr[j] = 0;
 		}
