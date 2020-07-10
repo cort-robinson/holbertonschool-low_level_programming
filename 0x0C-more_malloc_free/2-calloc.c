15508f801e02d9f4b4fba7c1dc7bb90979e8be98
cort-robinson <cortrobinson99@gmail.com>
2020-07-10 04:40:12 +0000

    add array initialization

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,7 +11,8 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr;
+	char *ptr;
+	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
@@ -19,5 +20,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	ptr = malloc(nmemb * sizeof(size));
 	if (ptr == NULL)
 		return (NULL);
+	for (i = 0; i < nmemb; i++)
+		ptr[i] = 0;
 	return (ptr);
 }
