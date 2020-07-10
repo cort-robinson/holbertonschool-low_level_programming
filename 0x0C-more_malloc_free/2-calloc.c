52d53a12d51eae7fed7425de8b4ec9cc5bfbeb89
cort-robinson <cortrobinson99@gmail.com>
2020-07-10 04:48:57 +0000

    mem allocation size fix

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -17,10 +17,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	ptr = malloc(nmemb * sizeof(size));
+	ptr = malloc(nmemb * size);
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; i < nmemb; i++)
+	for (i = 0; i < nmemb * size; i++)
 		ptr[i] = 0;
-	return ((void *)(ptr));
+	return (ptr);
 }
