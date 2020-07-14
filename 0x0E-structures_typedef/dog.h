524321d29541fc59ede86c6392546f58fa57cea0
cort-robinson <cortrobinson99@gmail.com>
2020-07-14 08:53:33 +0000

    betty fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,18 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - Structure for dog.
+ * @name: Name of dog.
+ * @age: Age of dog.
+ * @owner: Owner of dog.
+ */
+
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 } dog;
-
 
 #endif /* DOG_H */
