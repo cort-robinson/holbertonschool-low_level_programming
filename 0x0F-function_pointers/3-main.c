1bf1a278261d36376b81b753e709f5811e181a2f
cort-robinson <cortrobinson99@gmail.com>
2020-07-16 08:02:29 +0000

    Task 3 update

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -7,12 +7,22 @@
  * Return: 0 if no errors, else 1;
  */
 
-int main(int argc, char *argv[])
+int main(void)
 {
+	int (*op)(int, int), n1, n2, argc = 4;
+	char *argv[] = {"./a.out", "25", "/", "5"};
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit (98);
 	}
+	
+	op =  get_op_func(argv[2]);
 
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[3]);
+	
+	printf("%d\n", op(n1, n2));
+	return (0);
 }
