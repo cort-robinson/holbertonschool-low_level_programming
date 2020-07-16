e1fac78316be6c85101f63d0db21cb239efd772a
cort-robinson <cortrobinson99@gmail.com>
2020-07-16 03:49:30 +0000

    array index error fix

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -15,8 +15,6 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 
 	if (array == NULL || size < 1 || action == NULL)
 		return;
-	for (i = 0; i <= size; i++)
-	{
+	for (i = 0; i < size; i++)
 		action(array[i]);
-	}
 }
