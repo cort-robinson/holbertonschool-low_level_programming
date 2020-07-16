4435e7304ce735ea7eed0205e87db578693fef72
cort-robinson <cortrobinson99@gmail.com>
2020-07-16 05:55:18 +0000

    add files for task 3

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,18 @@
+#include "3-calc.h"
+
+/**
+ * main - Performs simple math operation.
+ * @argc: Number of arguments.
+ * @argv: Array of arguments given.
+ * Return: 0 if no errors, else 1;
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit (98);
+	}
+
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,65 @@
+#include "3-calc.h"
+
+/**
+ * op_add - Returns the sum of a and b.
+ * @a: Int 1.
+ * @b: Int 2.
+ * Return: Sum of a + b.
+ */
+
+int op_add(int a, int b);
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Returns the difference of a and b.
+ * @a: Int 1.
+ * @b: Int 2.
+ * Return: Difference of a and b.
+ */
+
+int op_sub(int a, int b)
+{
+	if (a >= b)
+		return (a - b);
+	return (b - a);
+}
+
+/**
+ * op_mul - Multiplies a and b.
+ * @a: Int 1.
+ * @b: Int 2.
+ * Return: Product of a and b.
+ */
+
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * op_div - Divides a by b.
+ * @a: Int 1.
+ * @b: Int 2.
+ * Return: a divided by b.
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Returns remainder of
+ * division of a by b.
+ * @a: Int 1.
+ * @b: Int 2.
+ * Return: Remainder of division
+ * of a by b.
+ */
+
+int op_mod(int a, int b)
+{
+	return (a % b);
+}
