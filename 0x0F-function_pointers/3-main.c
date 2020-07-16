224fa655cebb96660fc089ee6982babd5529217e
cort-robinson <cortrobinson99@gmail.com>
2020-07-16 09:25:07 +0000

    3-main.c update

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -9,7 +9,8 @@
 
 int main(int argc, char *argv[])
 {
-	int (*op)(int, int), n1, n2;
+	int (*op)(int, int), n1, n2, i;
+	char ops[] = {"+", "-", "*", "/", "%"};
 
 	if (argc != 4)
 	{
@@ -18,9 +19,25 @@ int main(int argc, char *argv[])
 	}
 	
 	op =  get_op_func(argv[2]);
+	for (i = 0; ops[i] ; i++)
+	{
+		if (op == ops[i])
+			break;
+	}
+	if (!ops[i])
+	{
+		printf("Error\n");
+		exit (99);
+	}
 
 	n1 = atoi(argv[1]);
 	n2 = atoi(argv[3]);
+
+	if ((op == "/" || op == "%") && n2 == 0)
+	{
+		printf("Error\n");
+		exit (100);
+	}
 	
 	printf("%d\n", op(n1, n2));
 	return (0);
