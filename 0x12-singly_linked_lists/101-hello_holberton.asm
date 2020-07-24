2381024b135c1e31fdf9f4c59ffbe58037890f0f
cort-robinson <cortrobinson99@gmail.com>
2020-07-24 19:24:44 +0000

    change 101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,26 +1,28 @@
-	.file	"101-hello_holberton.c"
-	.section	.rodata
-.LC0:
-	.string	"Hello, Holberton"
-	.text
-	.globl	main
-	.type	main, @function
-main:
-.LFB0:
-	.cfi_startproc
-	pushq	%rbp
-	.cfi_def_cfa_offset 16
-	.cfi_offset 6, -16
-	movq	%rsp, %rbp
-	.cfi_def_cfa_register 6
-	movl	$.LC0, %edi
-	call	puts
-	movl	$0, %eax
-	popq	%rbp
-	.cfi_def_cfa 7, 8
-	ret
-	.cfi_endproc
-.LFE0:
-	.size	main, .-main
-	.ident	"GCC: (Ubuntu 4.8.4-2ubuntu1~14.04.4) 4.8.4"
-	.section	.note.GNU-stack,"",@progbits
+
+
+section .text                   ;section declaration
+
+                                ;we must export the entry point to the ELF linker or
+    global  _start              ;loader. They conventionally recognize _start as their
+			                          ;entry point. Use ld -e foo to override the default.
+
+_start:
+
+                                ;write our string to stdout
+
+    mov     edx,len             ;third argument: message length
+    mov     ecx,msg             ;second argument: pointer to message to write
+    mov     ebx,1               ;first argument: file handle (stdout)
+    mov     eax,4               ;system call number (sys_write)
+    int     0x80                ;call kernel
+
+                                ;and exit
+
+  	mov     ebx,0               ;first syscall argument: exit code
+    mov     eax,1               ;system call number (sys_exit)
+    int     0x80                ;call kernel
+
+section .data                   ;section declaration
+
+msg db      "Hello, world!",0xa ;our dear string
+len equ     $ - msg             ;length of our dear string
