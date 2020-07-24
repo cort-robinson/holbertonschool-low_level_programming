2f76f602f486c8ce99956f879b52339b14b2d19c
cort-robinson <cortrobinson99@gmail.com>
2020-07-24 04:11:04 +0000

    add 1-list_len.c

---
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * list_len - Returns the number of elements
+ * in a linked list_t list.
+ * @h: First node of list.
+ * Return: Number of elements in linked list_t list.
+ */
+
+size_t list_len(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		i++;
+		h = h->next;
+	}
+	return (i);
+}
