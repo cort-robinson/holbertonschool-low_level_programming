6e0250b42e89a73cd50aa6a0e5886419548e3d7b
cort-robinson <cortrobinson99@gmail.com>
2020-07-29 04:54:47 +0000

    finish 6-pop_listint.c

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -9,5 +9,14 @@
 
 int pop_listint(listint_t **head)
 {
+	int n;
+	listint_t *tmp;
 
+	if (*head == NULL)
+		return (0);
+	n = (*head)->n;
+	tmp = *head;
+	*head = (*head)->next;
+	free(tmp);
+	return (n);
 }
