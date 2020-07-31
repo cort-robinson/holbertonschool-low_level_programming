1f67d361495f791ef633cfc578e2eb333edeacbb
cort-robinson <cortrobinson99@gmail.com>
2020-07-31 03:56:11 +0000

    change 0-binary_to_uint.c for user-defined strlen

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,12 +9,13 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int num = 0;
-	int base = 1, i;
+	int base = 1, i = 0;
 
 	if (b == NULL)
 		return (0);
-
-	for (i = strlen(b) - 1; i >= 0; i--)
+	while (b[i] != '\0')
+		i++;
+	for (i = i - 1; i >= 0; i--)
 	{
 		if (b[i] != '1' && b[i] != '0')
 			return (0);
