e88b63a819478863b469c3848c9f27b85c1be71e
cort-robinson <cortrobinson99@gmail.com>
2020-08-05 05:36:44 +0000

    add filename null check

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,6 +11,9 @@ int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd, wr, text_len;
 
+	if (filename == NULL)
+		return (-1);
+
 	fd = open(filename, O_RDWR | O_APPEND);
 	if (fd == -1)
 		return (-1);
