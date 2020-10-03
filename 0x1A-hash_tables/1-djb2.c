532552eb44dcfcbe4685fe27b7c3f85849a7415b
cort-robinson <cortrobinson99@gmail.com>
2020-10-03 00:18:35 +0000

    add file and fix includes

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,4 +1,4 @@
-
+#include "hash_tables.h"
 
 /**
  * hash_djb2 - implements djb2 algorithm.
---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,14 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - Returns index of key.
+ * @key: String to implement in hash.
+ * @size: Size of index.
+ *
+ * Return: 
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
