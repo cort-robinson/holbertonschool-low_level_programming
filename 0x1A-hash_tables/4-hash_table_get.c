e29ed4e2a9e8c2e6ddd0da57457b85f1141a4ace
cort-robinson <cortrobinson99@gmail.com>
2020-10-03 00:30:22 +0000

    add file

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - Retrieves a value associated with a key.
+ * @ht: Hash table to get from.
+ * @key: Key to look for.
+ *
+ * Return: Value associated with element or NULL if key not found.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+
+}
