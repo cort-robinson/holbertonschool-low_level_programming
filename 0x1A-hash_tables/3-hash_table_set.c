836d5c92b0e5880193f569de3d6ea49d238271e6
cort-robinson <cortrobinson99@gmail.com>
2020-10-03 00:29:02 +0000

    add file

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,15 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Adds an element to the hash table.
+ * @ht: Hash table to modify.
+ * @key: Key to add.
+ * @value: Value associated with key.
+ *
+ * Return: 1 on success, else 0.
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+
+}
