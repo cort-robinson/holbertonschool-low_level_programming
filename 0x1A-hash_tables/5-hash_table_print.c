1bf56d8c5ad697bb5497029ebd0da2330aded4e3
cort-robinson <cortrobinson99@gmail.com>
2020-10-08 21:00:46 +0000

    add NULL check for ht

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -11,6 +11,8 @@ void hash_table_print(const hash_table_t *ht)
 	hash_node_t *p;
     char *sep = "";
 
+    if (ht == NULL)
+        return;
     printf("{");
 	for (i = 0; ht && i < ht->size; i++)
 	{
