bca28eca9abc0917bbde0c814f7ae6c2a47ea207
cort-robinson <cortrobinson99@gmail.com>
2020-10-08 22:23:46 +0000

    fix memory issues

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -29,8 +29,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(tmp->key, key) == 0)
 		{
-			new->value = strdup(value);
-			free(new->value);
+			strcpy(tmp->value, value);
 			free(new);
 			return (1);
 		}
