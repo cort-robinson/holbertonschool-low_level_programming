ef0d90fa4a79b7fa141cce936df04230f6f012aa
cort-robinson <cortrobinson99@gmail.com>
2020-10-08 20:24:22 +0000

    betty fixes

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,17 +13,17 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int idx;
 	hash_node_t *head;
 
-    if (key == NULL || ht == NULL || strcmp(key, "") == 0)
+	if (key == NULL || ht == NULL || strcmp(key, "") == 0)
 		return (0);
 
-    idx = key_index((const unsigned char *)key, ht->size);
-    head = ht->array[idx];
-    
-    while (head)
-    {
-        if (strcmp(head->key, key) == 0)
-            return (head->value);
-        head = head->next;
-    }
-    return (NULL);
+	idx = key_index((const unsigned char *)key, ht->size);
+	head = ht->array[idx];
+
+	while (head)
+	{
+		if (strcmp(head->key, key) == 0)
+			return (head->value);
+		head = head->next;
+	}
+	return (NULL);
 }
