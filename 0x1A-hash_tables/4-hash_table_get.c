5822b71f83259344a1abbae40dc51968c7ed61c0
cort-robinson <cortrobinson99@gmail.com>
2020-10-08 20:23:40 +0000

    add file

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -7,7 +7,23 @@
  *
  * Return: Value associated with element or NULL if key not found.
  */
+
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
+	unsigned long int idx;
+	hash_node_t *head;
 
+    if (key == NULL || ht == NULL || strcmp(key, "") == 0)
+		return (0);
+
+    idx = key_index((const unsigned char *)key, ht->size);
+    head = ht->array[idx];
+    
+    while (head)
+    {
+        if (strcmp(head->key, key) == 0)
+            return (head->value);
+        head = head->next;
+    }
+    return (NULL);
 }
