c3764b7aaaa00c63b003f586c8c2874caef4513b
cort-robinson <cortrobinson99@gmail.com>
2020-10-08 21:02:11 +0000

    betty fixes

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -7,22 +7,22 @@
 
 void hash_table_print(const hash_table_t *ht)
 {
-    unsigned long int i;
+	unsigned long int i;
 	hash_node_t *p;
-    char *sep = "";
+	char *sep = "";
 
-    if (ht == NULL)
-        return;
-    printf("{");
+	if (ht == NULL)
+		return;
+	printf("{");
 	for (i = 0; ht && i < ht->size; i++)
 	{
 		p = ht->array[i];
 		while (p)
 		{
 			printf("%s'%s': '%s'", sep, p->key, p->value);
-			p = p-> next;
-            sep = ", ";
+			p = p->next;
+			sep = ", ";
 		}
 	}
-    printf("}\n");
+	printf("}\n");
 }
