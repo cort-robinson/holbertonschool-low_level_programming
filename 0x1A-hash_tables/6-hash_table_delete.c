233c8202daf40d5a1f6b99f44ad7216db6c7bc2d
cort-robinson <cortrobinson99@gmail.com>
2020-10-08 21:38:42 +0000

    some fixes

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -7,5 +7,21 @@
 
 void hash_table_delete(hash_table_t *ht)
 {
+	unsigned long int i;
+	hash_node_t *p, *next;
 
+	for (i = 0; ht && i < ht->size; i++)
+	{
+		p = ht->array[i];
+		while (p)
+		{
+			next = p->next;
+			free(p->key);
+			free(p->value);
+			free(p);
+			p = next;
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
