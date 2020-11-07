bbb0021706b35e7c08801a0b9c8b2c59323bced3
cort-robinson <cortrobinson99@gmail.com>
2020-11-07 03:30:30 +0000

    fix looping and perimiter counting

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,15 +5,15 @@
 def island_perimeter(grid):
     """returns perimiter of island stored in grid"""
     perimeter = 0
-    for x, row in enumerate(grid):
-        for y, zone in enumerate(row):
-            if zone == 1:
-                if grid[x - 1][y] == 0:
+    for x in range(len(grid)):
+        for y in range(len(grid[x])):
+            if grid[x][y] == 1:
+                if (x - 1) < 0 or grid[x - 1][y] == 0:
                     perimeter += 1
-                if (y - 1) >= 0 and grid[x][y - 1] == 0:
+                if (y - 1) < 0 or grid[x][y - 1] == 0:
                     perimeter += 1
-                if (x + 1) < len(grid) and grid[x + 1][y] == 0:
+                if (x + 1) == len(grid) or grid[x + 1][y] == 0:
                     perimeter += 1
-                if (y + 1) < len(grid[x]) and grid[x][y + 1] == 0:
+                if (y + 1) == len(grid[x]) or grid[x][y + 1] == 0:
                     perimeter += 1
     return perimeter
